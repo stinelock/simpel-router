958582ced75314406e25c4a710a07845869c9d24
stinelock <eaa24stlo@students.eaaa.dk>
2025-08-28 15:11:09 +0200

    fix: correct typo in routing base path

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/simple-router/"}>
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/simpel-router/"}>
       <App />
     </BrowserRouter>
   </StrictMode>
